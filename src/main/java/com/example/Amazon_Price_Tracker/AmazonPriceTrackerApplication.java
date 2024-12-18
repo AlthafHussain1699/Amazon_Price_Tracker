240e49003c65e3af637501a3de32ac36581e6999
AlthafHussain <althafhussain1699@gmail.com>
2024-12-18 09:31:26 +0530

    first commit

---
+++ src/main/java/com/example/Amazon_Price_Tracker/AmazonPriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Amazon_Price_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonPriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonPriceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Amazon_Price_Tracker/AmazonPriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Amazon_Price_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonPriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
